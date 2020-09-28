a4dc71f7be994c031d9b8d4804899b0fe903041e
Chris Simpson <chris_simpson@live.com.au>
2020-09-28 17:31:51 +0000

    modified drawCards function to seperate cardFace and cardBack images into nested divs within card div to make it easier for me to style later

--- app.js
+++ app.js
@@ -18,21 +18,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //loop over cardArray and deploy to DOM
     function drawCards() {
+        //loop over cardArray
         for (i = 0; i < cardArray.length; i++) {
+            //create card divs
             var card = document.createElement('div')
+            var cardFaceDiv = document.createElement('div')
+            var cardBackDiv = document.createElement('div')
+            card.classList.add('card')
+            //create images to be placed in card divs
             var cardFace = document.createElement('img')
             var cardBack = document.createElement('img')
+            //pull img and name objects from cardArray and apply to cardFace. Apply extra data to use for matching and switcheable classes for selecting
             cardFace.setAttribute('src', cardArray[i].img)
             cardFace.setAttribute('name', cardArray[i].name)
             cardFace.setAttribute('card-id', i)
-            cardFace.classList.add('cardhide')
+            cardFace.classList.add('cardFace', 'cardhide')
+            //set cardBack image as default
             cardBack.setAttribute('src', 'assets/images/ball.png')
-            cardBack.classList.add('cardshow')
-            card.appendChild(cardFace)
-            card.appendChild(cardBack)
+            cardBack.classList.add('cardBack', 'cardshow')
+            //apply images to card divs
+            cardFaceDiv.appendChild(cardFace)
+            cardBackDiv.appendChild(cardBack)
+            //apply card divs to game div
+            card.appendChild(cardFaceDiv)
+            card.appendChild(cardBackDiv)
             game.appendChild(card)
         }
     }
+
+    
 
     drawCards()
 })
